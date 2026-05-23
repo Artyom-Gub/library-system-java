37856a935deaa65bb966013fa63f4a66c3313a12
Artyom <ar.gubaydullin0231@gmail.com>
2026-05-24 01:24:54 +0500

    Create simple main func which shows book menu

---
+++ src/www/main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "book";
+
+        /*template for book menu*/
+        System.out.println("Books: ");
+        System.out.println(str + "1");
+        System.out.println(str + "2");
+        System.out.println(str + "3");
+    }
+}
